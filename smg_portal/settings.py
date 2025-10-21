15b28f653219e2c5632e4604eae68692c5d80223
PriyanshuVe <priyanshuverma672003@gmail.com>
2025-10-21 18:00:55 +0530

    Fixed allowed hosts and CSRF trusted origins

--- smg_portal/settings.py
+++ smg_portal/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'dev-secret-please-change')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = ["https://smg-service-portal.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -130,3 +130,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://smg-service-portal.onrender.com"
+]
